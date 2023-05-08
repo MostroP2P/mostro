dc6b167db50050b6cb690a3d45f23ac3a3a6398d
Francisco Calderón <fjcalderon@gmail.com>
2023-05-08 19:19:44 -0300

    Bug fixes on scheduler and refactoring (#73)
    * Bug fixes on scheduler and refactoring
    
    * sqlx prepare file

--- src/db.rs
+++ src/db.rs
@@ -559,3 +559,26 @@ pub async fn reset_order_taken_at_time(pool: &SqlitePool, order_id: Uuid) -> any
 
     Ok(rows_affected > 0)
 }
+
+pub async fn update_order_invoice_held_at_time(
+    pool: &SqlitePool,
+    order_id: Uuid,
+    invoice_held_at: i64,
+) -> anyhow::Result<bool> {
+    let mut conn = pool.acquire().await?;
+    let rows_affected = sqlx::query!(
+        r#"
+            UPDATE orders
+            SET
+            invoice_held_at = ?1
+            WHERE id = ?2
+        "#,
+        invoice_held_at,
+        order_id,
+    )
+    .execute(&mut conn)
+    .await?
+    .rows_affected();
+
+    Ok(rows_affected > 0)
+}

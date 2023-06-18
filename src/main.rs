526f4025809358608b18f36c72ba9532a478c444
Arkanoider <github.913zc@simplelogin.com>
2023-06-18 10:10:06 +0200

    cargo fmt and small fix for missing .env file

--- src/main.rs
+++ src/main.rs
@@ -52,12 +52,11 @@ async fn main() -> Result<()> {
 
 #[cfg(test)]
 mod tests {
+    use dotenvy::dotenv;
     use mostro_core::order::NewOrder;
     use mostro_core::Message;
     use std::env::var;
     use std::time::{SystemTime, UNIX_EPOCH};
-    use dotenvy::dotenv;
-    
 
     #[test]
     fn test_order_deserialize_serialize() {
@@ -86,30 +85,33 @@ mod tests {
     #[test]
     fn test_fee_rounding() {
         dotenv().ok();
-        let fee = var("FEE").unwrap().parse::<f64>().unwrap_or(0.003) / 2.0;
+        let fee = var("FEE")
+            .unwrap_or(0.003.to_string())
+            .parse::<f64>()
+            .unwrap_or(0.003)
+            / 2.0;
 
         let mut amt = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .subsec_micros() as i64;
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .subsec_micros() as i64;
 
         // Test 1000 "random" amounts
         for _i in 1..=1000 {
-
             let fee_calculated = fee * amt as f64;
             let rounded_fee = fee_calculated.round();
             // Seller side
             let seller_total_amt = rounded_fee as i64 + amt;
-            assert_eq!(amt, seller_total_amt-rounded_fee as i64);
+            assert_eq!(amt, seller_total_amt - rounded_fee as i64);
             // Buyer side
-           
-            let buyer_total_amt = amt -rounded_fee as i64;
+
+            let buyer_total_amt = amt - rounded_fee as i64;
             assert_eq!(amt, buyer_total_amt + rounded_fee as i64);
 
-            let  nonce = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .subsec_millis() as i64;
+            let nonce = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .subsec_millis() as i64;
 
             amt %= 100_000_i64;
             amt *= (rounded_fee as i64) % 100_i64;

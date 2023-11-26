42950bc0cd5a9f8d8f69d6367bf6b8298c9c8090
Arkanoider <github.913zc@simplelogin.com>
2023-11-26 11:11:26 +0100

    Test for workaround of offline sqlx json file for github tests

--- init_db.sh
+++ init_db.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+echo "Clean project"
+if ls Cargo.lock 1> /dev/null 2>&1; then
+    echo "Deleting Cargo.lock"
+    rm -rf ./Cargo.lock
+fi
+if ls sqlx-data.json 1> /dev/null 2>&1; then
+    echo "Deleting old sqlx-data.json"
+    rm -rf ./sqlx-data.json
+fi
+
+cargo clean
 echo "Reading database URL from settings.toml..."
 DATABASE_URL=$(awk -F'"' '/url *= */ {print $2}' settings.tpl.toml)
 export DATABASE_URL
@@ -13,4 +24,9 @@ echo "Creating new database..."
 sqlx database create
 echo "Running migrations..."
 sqlx migrate run
-echo "Done!"
+echo "Preparing offline file for CI on github!"
+cargo sqlx prepare
+echo "Check json db file is ok!"
+cargo sqlx prepare --check
+
+

cd403f971bb400370950ee7ba4affe02dd02d91c
arkanoider <github.913zc@simplelogin.com>
2024-02-06 08:55:02 +0100

    correct fix to avoid dispute with the same order id

--- migrations/20230928145530_disputes.sql
+++ migrations/20230928145530_disputes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS disputes (
   id char(36) primary key not null,
-  order_id char(36) not null,
+  order_id char(36) unique not null,
   status varchar(10) not null,
   solver_pubkey char(64),
   created_at integer not null,

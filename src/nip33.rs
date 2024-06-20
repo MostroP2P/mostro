93f102b1e9e43115888986822a839c5d301323b6
Francisco Calderón <fjcalderon@gmail.com>
2024-06-20 12:30:31 -0300

    Syntax fix

--- src/nip33.rs
+++ src/nip33.rs
@@ -125,72 +125,60 @@ pub fn info_to_tags(mostro_pubkey: &PublicKey) -> Vec<Tag> {
     let mostro_settings = Settings::get_mostro();
     let ln_settings = Settings::get_ln();
 
-    let mut tags: Vec<Tag> = vec![];
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("mostro_pubkey")),
-        vec![mostro_pubkey.to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("mostro_version")),
-        vec![env!("CARGO_PKG_VERSION").to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("mostro_commit_id")),
-        vec![env!("GIT_HASH").to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("max_order_amount")),
-        vec![mostro_settings.max_order_amount.to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("min_order_amount")),
-        vec![mostro_settings.min_payment_amount.to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("expiration_hours")),
-        vec![mostro_settings.expiration_hours.to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("expiration_seconds")),
-        vec![mostro_settings.expiration_seconds.to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("fee")),
-        vec![mostro_settings.fee.to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("hold_invoice_expiration_window")),
-        vec![ln_settings.hold_invoice_expiration_window.to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("hold_invoice_cltv_delta")),
-        vec![ln_settings.hold_invoice_cltv_delta.to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("invoice_expiration_window")),
-        vec![ln_settings.hold_invoice_expiration_window.to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("y")),
-        vec!["mostrop2p".to_string()],
-    ));
-
-    tags.push(Tag::custom(
-        TagKind::Custom(std::borrow::Cow::Borrowed("z")),
-        vec!["info".to_string()],
-    ));
+    let tags: Vec<Tag> = vec![
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("mostro_pubkey")),
+            vec![mostro_pubkey.to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("mostro_version")),
+            vec![env!("CARGO_PKG_VERSION").to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("mostro_commit_id")),
+            vec![env!("GIT_HASH").to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("max_order_amount")),
+            vec![mostro_settings.max_order_amount.to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("min_order_amount")),
+            vec![mostro_settings.min_payment_amount.to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("expiration_hours")),
+            vec![mostro_settings.expiration_hours.to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("expiration_seconds")),
+            vec![mostro_settings.expiration_seconds.to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("fee")),
+            vec![mostro_settings.fee.to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("hold_invoice_expiration_window")),
+            vec![ln_settings.hold_invoice_expiration_window.to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("hold_invoice_cltv_delta")),
+            vec![ln_settings.hold_invoice_cltv_delta.to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("invoice_expiration_window")),
+            vec![ln_settings.hold_invoice_expiration_window.to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("y")),
+            vec!["mostrop2p".to_string()],
+        ),
+        Tag::custom(
+            TagKind::Custom(std::borrow::Cow::Borrowed("z")),
+            vec!["info".to_string()],
+        ),
+    ];
 
     tags
 }

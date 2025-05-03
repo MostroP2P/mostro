4440511ac77326b3b087ba5d17810d6b40eeecc4
arkanoider <github.913zc@simplelogin.com>
2025-05-03 13:23:41 +0200

    Add some comments for docs on data structures

--- src/config/types.rs
+++ src/config/types.rs
@@ -18,42 +18,65 @@ macro_rules! impl_try_from_settings {
         )*
     };
 }
-
+/// Database configuration settings
 #[derive(Debug, Deserialize, Default, Clone)]
 pub struct Database {
+    /// Database connection URL (e.g., "postgres://user:pass@localhost/dbname")  
     pub url: String,
 }
-
+/// Lightning configuration settings
 #[derive(Debug, Deserialize, Default, Clone)]
 pub struct Lightning {
+    /// LND certificate file path
     pub lnd_cert_file: String,
+    /// LND macaroon file path
     pub lnd_macaroon_file: String,
+    /// LND gRPC host
     pub lnd_grpc_host: String,
+    /// Invoice expiration window in seconds
     pub invoice_expiration_window: u32,
+    /// Hold invoice CLTV delta
     pub hold_invoice_cltv_delta: u32,
+    /// Hold invoice expiration window in seconds
     pub hold_invoice_expiration_window: u32,
+    /// Number of payment attempts
     pub payment_attempts: u32,
+    /// Payment retries interval in seconds
     pub payment_retries_interval: u32,
 }
-
+/// Nostr configuration settings
 #[derive(Debug, Deserialize, Default, Clone)]
 pub struct Nostr {
+    /// Nostr private key
     pub nsec_privkey: String,
+    /// Nostr relays list
     pub relays: Vec<String>,
 }
+/// Mostro configuration settings
 
 #[derive(Debug, Deserialize, Default, Clone)]
 pub struct Mostro {
+    /// Fee percentage for the Mostro
     pub fee: f64,
+    /// Maximum routing fee percentage
     pub max_routing_fee: f64,
+    /// Maximum order amount
     pub max_order_amount: u32,
+    /// Minimum payment amount
     pub min_payment_amount: u32,
+    /// Expiration hours
     pub expiration_hours: u32,
+    /// Expiration seconds
     pub expiration_seconds: u32,
+    /// User rates sent interval seconds
     pub user_rates_sent_interval_seconds: u32,
+    /// Maximum expiration days
     pub max_expiration_days: u32,
+    /// Publish relays interval
     pub publish_relays_interval: u32,
+    /// Proof of work required
     pub pow: u8,
+    /// Publish mostro info interval
     pub publish_mostro_info_interval: u32,
 }
 

07c5bbd1cac85cd25a102de1e9694e4e18af7bf4
arkanoider <github.913zc@simplelogin.com>
2025-08-18 18:54:52 +0200

    chore: fix test zero amount invoice

--- src/lightning/invoice.rs
+++ src/lightning/invoice.rs
@@ -285,7 +285,7 @@ mod tests {
     #[tokio::test]
     async fn test_zero_amount_invoice() {
         init_settings_test();
-        let payment_request = "lnbc1p5fr5rppp57geae89p9quvwgedaul0x3gpedhrmnh9uwxdwdtja5t9dteaqphqcqzyssp5aftk78kzcnej33f7eydgta9x5wup3v6m26hu4upgvr7709fg0mxq9q7sqqqqqqqqqqqqqqqqqqqsqqqqqysgqdqqmqz9gxqyjw5qrzjqwryaup9lh50kkranzgcdnn2fgvx390wgj5jd07rwr3vxeje0glclludlw6z8nzdzcqqqqlgqqqqqeqqjqsatwvdm6hcwgmxx5pjz0wl6m6s9f8rrupeh97vesnt54exasemxk7a4knjgc6xhy9qrc3e78y70yxenaymykyw0crkwgerhf3y7zpfqqd8wnvl".to_string();
+        let payment_request = "lnbc1p52xk52pp53crvcz8x52sqvjwrryhuspqgtjfveuut2hvw8ftu0gazcd6l3hasdq5g9kxy7fqd9h8vmmfvdjscqzzsxqyz5vqsp5sqkze6ay30rmv4w52pk7cku9pl7xmhr9neytpstseka4spa0yxps9qxpqysgql66l5vh4dsdj4awunq8t83v3ayjynrrlx7tsh5npvsz87gk0wsczxgrtmgh499p6nzum8dywekajq5h8lhtp6xlpvm0a8jmu750usyspe3vqz0".to_string();
         let zero_amount_err = is_valid_invoice(payment_request, Some(100), None);
         assert_eq!(Ok(()), zero_amount_err.await);
     }

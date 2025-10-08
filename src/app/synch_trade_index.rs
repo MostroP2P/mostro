aa3ae395a96e2246aacd0752d0dc6a4d7ca1cd42
arkanoider <github.913zc@simplelogin.com>
2025-10-08 16:46:12 +0200

    refactor: changed code to use new LastTradeIndex message introduced in the protocol

--- src/app/synch_trade_index.rs
+++ src/app/synch_trade_index.rs
@@ -6,8 +6,7 @@ use nostr_sdk::prelude::*;
 use sqlx::{Pool, Sqlite};
 
 // Handle synch_user_trade_index action
-pub async fn synch_user_trade_index_action(
-    msg: Message,
+pub async fn synch_last_trade_index(
     event: &UnwrappedGift,
     my_keys: &Keys,
     pool: &Pool<Sqlite>,
@@ -19,18 +18,18 @@ pub async fn synch_user_trade_index_action(
     let user = is_user_present(pool, requester_pubkey).await?;
 
     // Build response message embedding the last_trade_index in the trade_index field
-    let response = Message::new_order(
+    let last_trade_idx_message = MessageKind::new(
         None,
-        msg.get_inner_message_kind().request_id,
+        None,
         Some(user.last_trade_index),
-        Action::SynchUserTradeIndex,
+        Action::LastTradeIndex,
         None,
     )
     .as_json()
     .map_err(|_| MostroError::MostroInternalErr(ServiceError::MessageSerializationError))?;
 
     // Send DM back to the requester
-    send_dm(event.sender, my_keys, &response, None).await?;
+    send_dm(event.sender, my_keys, &last_trade_idx_message, None).await?;
 
     Ok(())
 }
